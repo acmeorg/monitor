6e2ce58421927d51e8fd705c25927752a38ac18a
Haris Delalic <haris.delalic@versionone.com>
2024-06-15 18:46:32 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let language = 'bs'; // Initial language is Bosnian
 
 const schedules = {
-    sunday: [
+    weekdays: [
         { time: "04:40", destination: "Zenica", train: "2150", type: "lokalni", platform: "I", track: "1" },
         { time: "06:30", destination: "Banja Luka", train: "712", type: "brzi", platform: "I", track: "1" },
         { time: "07:07", destination: "Čapljina", train: "723", type: "brzi", platform: "II", track: "2" },
@@ -13,77 +13,16 @@ const schedules = {
         { time: "16:50", destination: "Čapljina", train: "721", type: "brzi", platform: "I", track: "1" },
         { time: "19:20", destination: "Zenica", train: "2158", type: "lokalni", platform: "II", track: "3" }
     ],
-    monday: [
-        { time: "04:40", destination: "Zenica", train: "2150", type: "lokalni", platform: "I", track: "1" },
-        { time: "06:30", destination: "Banja Luka", train: "712", type: "brzi", platform: "I", track: "1" },
-        { time: "07:07", destination: "Čapljina", train: "723", type: "brzi", platform: "II", track: "2" },
-        { time: "07:14", destination: "Zenica", train: "2152", type: "lokalni", platform: "II", track: "3" },
-        { time: "10:56", destination: "Zenica", train: "2154", type: "lokalni", platform: "II", track: "3" },
-        { time: "16:00", destination: "Bihać", train: "714", type: "brzi", platform: "I", track: "1" },
-        { time: "16:07", destination: "Konjic", train: "2401", type: "lokalni", platform: "II", track: "2" },
-        { time: "16:13", destination: "Kakanj", train: "2156", type: "lokalni", platform: "II", track: "3" },
-        { time: "16:50", destination: "Čapljina", train: "721", type: "brzi", platform: "I", track: "1" },
-        { time: "19:20", destination: "Zenica", train: "2158", type: "lokalni", platform: "II", track: "3" }
-    ],
-    tuesday: [
-        { time: "04:40", destination: "Zenica", train: "2150", type: "lokalni", platform: "I", track: "1" },
-        { time: "06:30", destination: "Banja Luka", train: "712", type: "brzi", platform: "I", track: "1" },
-        { time: "07:07", destination: "Čapljina", train: "723", type: "brzi", platform: "II", track: "2" },
-        { time: "07:14", destination: "Zenica", train: "2152", type: "lokalni", platform: "II", track: "3" },
-        { time: "10:56", destination: "Zenica", train: "2154", type: "lokalni", platform: "II", track: "3" },
-        { time: "16:00", destination: "Bihać", train: "714", type: "brzi", platform: "I", track: "1" },
-        { time: "16:07", destination: "Konjic", train: "2401", type: "lokalni", platform: "II", track: "2" },
-        { time: "16:13", destination: "Kakanj", train: "2156", type: "lokalni", platform: "II", track: "3" },
-        { time: "16:50", destination: "Čapljina", train: "721", type: "brzi", platform: "I", track: "1" },
-        { time: "19:20", destination: "Zenica", train: "2158", type: "lokalni", platform: "II", track: "3" }
-    ],
-    wednesday: [
-        { time: "04:40", destination: "Zenica", train: "2150", type: "lokalni", platform: "I", track: "1" },
-        { time: "06:30", destination: "Banja Luka", train: "712", type: "brzi", platform: "I", track: "1" },
-        { time: "07:07", destination: "Čapljina", train: "723", type: "brzi", platform: "II", track: "2" },
-        { time: "07:14", destination: "Zenica", train: "2152", type: "lokalni", platform: "II", track: "3" },
-        { time: "10:56", destination: "Zenica", train: "2154", type: "lokalni", platform: "II", track: "3" },
-        { time: "16:00", destination: "Bihać", train: "714", type: "brzi", platform: "I", track: "1" },
-        { time: "16:07", destination: "Konjic", train: "2401", type: "lokalni", platform: "II", track: "2" },
-        { time: "16:13", destination: "Kakanj", train: "2156", type: "lokalni", platform: "II", track: "3" },
-        { time: "16:50", destination: "Čapljina", train: "721", type: "brzi", platform: "I", track: "1" },
-        { time: "19:20", destination: "Zenica", train: "2158", type: "lokalni", platform: "II", track: "3" }
-    ],
-    thursday: [
-        { time: "04:40", destination: "Zenica", train: "2150", type: "lokalni", platform: "I", track: "1" },
-        { time: "06:30", destination: "Banja Luka", train: "712", type: "brzi", platform: "I", track: "1" },
-        { time: "07:07", destination: "Čapljina", train: "723", type: "brzi", platform: "II", track: "2" },
-        { time: "07:14", destination: "Zenica", train: "2152", type: "lokalni", platform: "II", track: "3" },
-        { time: "10:56", destination: "Zenica", train: "2154", type: "lokalni", platform: "II", track: "3" },
-        { time: "16:00", destination: "Bihać", train: "714", type: "brzi", platform: "I", track: "1" },
-        { time: "16:07", destination: "Konjic", train: "2401", type: "lokalni", platform: "II", track: "2" },
-        { time: "16:13", destination: "Kakanj", train: "2156", type: "lokalni", platform: "II", track: "3" },
-        { time: "16:50", destination: "Čapljina", train: "721", type: "brzi", platform: "I", track: "1" },
-        { time: "19:20", destination: "Zenica", train: "2158", type: "lokalni", platform: "II", track: "3" }
-    ],
-    friday: [
-        { time: "04:40", destination: "Zenica", train: "2150", type: "lokalni", platform: "I", track: "1" },
-        { time: "06:30", destination: "Banja Luka", train: "712", type: "brzi", platform: "I", track: "1" },
-        { time: "07:07", destination: "Čapljina", train: "723", type: "brzi", platform: "II", track: "2" },
-        { time: "07:14", destination: "Zenica", train: "2152", type: "lokalni", platform: "II", track: "3" },
-        { time: "10:56", destination: "Zenica", train: "2154", type: "lokalni", platform: "II", track: "3" },
-        { time: "16:00", destination: "Bihać", train: "714", type: "brzi", platform: "I", track: "1" },
-        { time: "16:07", destination: "Konjic", train: "2401", type: "lokalni", platform: "II", track: "2" },
-        { time: "16:13", destination: "Kakanj", train: "2156", type: "lokalni", platform: "II", track: "3" },
-        { time: "16:50", destination: "Čapljina", train: "721", type: "brzi", platform: "I", track: "1" },
-        { time: "19:20", destination: "Zenica", train: "2158", type: "lokalni", platform: "II", track: "3" }
-    ],
-    saturday: [
-        { time: "04:40", destination: "Zenica", train: "2150", type: "lokalni", platform: "I", track: "1" },
-        { time: "06:30", destination: "Banja Luka", train: "712", type: "brzi", platform: "I", track: "1" },
-        { time: "07:07", destination: "Čapljina", train: "723", type: "brzi", platform: "II", track: "2" },
-        { time: "07:14", destination: "Zenica", train: "2152", type: "lokalni", platform: "II", track: "3" },
-        { time: "10:56", destination: "Zenica", train: "2154", type: "lokalni", platform: "II", track: "3" },
-        { time: "16:00", destination: "Bihać", train: "714", type: "brzi", platform: "I", track: "1" },
-        { time: "16:07", destination: "Konjic", train: "2401", type: "lokalni", platform: "II", track: "2" },
-        { time: "16:13", destination: "Kakanj", train: "2156", type: "lokalni", platform: "II", track: "3" },
-        { time: "16:50", destination: "Čapljina", train: "721", type: "brzi", platform: "I", track: "1" },
-        { time: "19:20", destination: "Zenica", train: "2158", type: "lokalni", platform: "II", track: "3" }
+    weekends: [
+        { time: "04:41", destination: "Zenica", train: "2150", type: "lokalni", platform: "I", track: "1" },
+        { time: "05:23", destination: "Visoko", train: "2160", type: "prigradski", platform: "II", track: "3" },
+        { time: "07:26", destination: "Pazarić", train: "2403", type: "prigradski", platform: "I", track: "1" },
+        { time: "10:21", destination: "Visoko", train: "2164", type: "prigradski", platform: "II", track: "3" },
+        { time: "11:55", destination: "Pazarić", train: "2407", type: "prigradski", platform: "I", track: "1" },
+        { time: "15:38", destination: "Zenica", train: "2156", type: "lokalni", platform: "II", track: "3" },
+        { time: "16:30", destination: "Maglaj", train: "710", type: "brzi", platform: "I", track: "1" },
+        { time: "16:46", destination: "Čapljina", train: "721", type: "brzi", platform: "II", track: "3" },
+        { time: "19:31", destination: "Pazarić", train: "2413", type: "prigradski", platform: "I", track: "1" }
     ]
 };
 
@@ -102,7 +41,6 @@ function getCurrentCETTime() {
 }
 
 function updateCurrentTime() {
-    const now = new Date();
     const cetTime = getCurrentCETTime();
     const hours = String(cetTime.hours).padStart(2, '0');
     const minutes = String(cetTime.minutes).padStart(2, '0');
@@ -116,10 +54,9 @@ function updateDepartures() {
     const currentMinute = cetTime.minutes;
     const departuresList = document.getElementById('departures-list');
 
-    // Determine the current day of the week
-    const daysOfWeek = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
-    const currentDay = daysOfWeek[new Date().getDay()];
-    const currentSchedule = schedules[currentDay];
+    // Determine if it's a weekday or weekend
+    const currentDay = new Date().getDay();
+    const currentSchedule = currentDay === 0 || currentDay === 6 ? schedules.weekends : schedules.weekdays;
 
     // Clear existing list items
     departuresList.innerHTML = '';
@@ -134,8 +71,8 @@ function updateDepartures() {
 
     // If no departures are left for the day, show the next day's schedule
     if (upcomingDepartures.length === 0) {
-        const nextDay = daysOfWeek[(new Date().getDay() + 1) % 7];
-        upcomingDepartures = schedules[nextDay].sort((a, b) => a.time.localeCompare(b.time));
+        const nextSchedule = currentDay === 5 ? schedules.weekends : schedules.weekdays;
+        upcomingDepartures = nextSchedule.sort((a, b) => a.time.localeCompare(b.time));
     }
 
     displayDepartures(upcomingDepartures, currentHour, currentMinute);
@@ -161,7 +98,6 @@ function displayDepartures(upcomingDepartures, currentHour, currentMinute) {
             <td>${departure.train}</td>
             <td class="train-type">${departure.type}</td>
             <td>${departure.platform}/${departure.track}</td>
-            <td></td> <!-- Delay column -->
         `;
         departuresList.appendChild(row);
     });
@@ -174,7 +110,6 @@ function toggleLanguage() {
     const destinationHeader = document.getElementById('destination-header');
     const trainHeader = document.getElementById('train-header');
     const platformTrackHeader = document.getElementById('platform-track-header');
-    const delayHeader = document.getElementById('delay-header');
     const trainTypes = document.querySelectorAll('.train-type');
 
     if (language === 'bs') {
@@ -184,13 +119,14 @@ function toggleLanguage() {
         destinationHeader.textContent = 'DESTINATION';
         trainHeader.textContent = 'TRAIN';
         platformTrackHeader.textContent = 'PLATFORM/TRACK';
-        delayHeader.textContent = 'DELAY';
 
         trainTypes.forEach(type => {
             if (type.textContent === 'brzi') {
                 type.textContent = 'express';
             } else if (type.textContent === 'lokalni') {
                 type.textContent = 'regional';
+            } else if (type.textContent === 'prigradski') {
+                type.textContent = 'commuter';
             }
         });
     } else {
@@ -200,13 +136,14 @@ function toggleLanguage() {
         destinationHeader.textContent = 'SMJER';
         trainHeader.textContent = 'VOZ';
         platformTrackHeader.textContent = 'PERON/KOL';
-        delayHeader.textContent = 'KASNI';
 
         trainTypes.forEach(type => {
             if (type.textContent === 'express') {
                 type.textContent = 'brzi';
             } else if (type.textContent === 'regional') {
                 type.textContent = 'lokalni';
+            } else if (type.textContent === 'commuter') {
+                type.textContent = 'prigradski';
             }
         });
     }
